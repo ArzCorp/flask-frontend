08a4c69e0aa89680ba25874e46abd8ee369a4acc
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-04-05 07:09:27 -0500

    🚧 request and response

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect
 
 app = Flask('_name_')
 
 @app.route('/')
 def hello ():
-  return 'Hello world'
+  user_ip = request.remote_addr
+  response = make_response(redirect("/home"))
+  response.set_cookie("user_ip", user_ip)
+  return response
+
+@app.route('/home')
+def Home():
+    user_ip = request.cookies.get("user_ip")
+    return "Home" + " " + user_ip
