40ed8fb5aa498dbd3ece029ea20c4f111a09fb3b
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-04-07 06:01:17 -0500

    🚧 session

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 
 app = Flask('_name_')
 
+app.config['SECRET_KEY'] = 'Osva-0807_App $'
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template("404.html", error=error)
+
 @app.route('/')
 def index ():
-  user_ip = request.remote_addr
-  response = make_response(redirect("/home"))
-  response.set_cookie("user_ip", user_ip)
-  return response
+    user_ip = request.remote_addr
+    response = make_response(redirect("/home"))
+    session['user_ip'] = user_ip
+    return response
 
 @app.route('/home')
 def home():
-    user_ip = request.cookies.get("user_ip")
+    user_ip = session.get('user_ip')
     response = render_template("home.html", user_ip=user_ip)
     return response
